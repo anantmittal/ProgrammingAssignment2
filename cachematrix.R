45bba1ea41df33199e4644ed3d54cac6589d06e8
Anant Mittal <mittal.anant@gmail.com>
2017-06-24 20:40:39 +0530

    Second Push

--- cachematrix.R
+++ cachematrix.R
@@ -4,22 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
-  }
-  get <- function() x
-  setinv <- function(solve) i <<- solve
-  getinv <- function() i
-  list(set = set, get = get, setinv = setinv, getinv = getinv)
+    i <- NULL
+    set <- function(y) {
+      x <<- y
+      i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) i <<- solve
+    getinv <- function() i
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)) {
+      message("getting cached data")
+      return(i)
+    }
+    data <- x$get()
+    i <- solve(data)
+    x$setinv(i)
+    i
 }
 
 
+A = matrix(c(1, 2, 3, 4),nrow=2,ncol=2,byrow = TRUE) 
+A
+solve(A)
+makeCacheMatrix(A)
+cacheSolve(makeCacheMatrix(A))
