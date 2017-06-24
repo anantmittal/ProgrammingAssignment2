fe8cb3b579345b090894a70df6128285b8d44f31
Anant Mittal <mittal.anant@gmail.com>
2017-06-24 20:48:35 +0530

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
 
-## Write a short comment describing this function
+## Matrix inversion is usually a costly computation 
+## and there may be some benefit to caching the inverse of a matrix
+## rather than compute it repeatedly 
+## (there are also alternatives to matrix inversion that we will not discuss here). 
+## Our assignment is to write a pair of functions that cache the inverse of a matrix.
+
+## The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the inverse of the matrix
+## get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
@@ -16,7 +25,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrix" created with the above function. 
+## However, it first checks to see if the inverse has already been calculated. 
+## If so, it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -32,8 +44,8 @@ cacheSolve <- function(x, ...) {
 }
 
 
-A = matrix(c(1, 2, 3, 4),nrow=2,ncol=2,byrow = TRUE) 
-A
-solve(A)
-makeCacheMatrix(A)
-cacheSolve(makeCacheMatrix(A))
+#A = matrix(c(1, 2, 3, 4),nrow=2,ncol=2,byrow = TRUE) 
+#A
+#solve(A)
+#makeCacheMatrix(A)
+#cacheSolve(makeCacheMatrix(A))
